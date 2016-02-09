3140d2a8e1471abf52eb1d47c51e224dd52d9d83
mharris13 <mharris13@wou.edu>
2016-02-08 16:05:02 -0800

    proj2

--- project2/assets/js/main.js
+++ project2/assets/js/main.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	
 	console.log("wassup");
 
+	$(function() {
+	 	$( "#garbage" ).draggable();
+  	});
+
+ 	$("#mouth").hover(function(){
+        $("#garbage").hide();
+    });
+
+ 
 });
