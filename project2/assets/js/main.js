0929cdc1cb816bdfc083bea9c330bc8c9677d184
mharris13 <mharris13@wou.edu>
2016-02-17 16:58:43 -0800

    proj2

--- project2/assets/js/main.js
+++ project2/assets/js/main.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
-	
-	console.log("wassup");
 
+	alert("Feed the Trump");
+
+	//functions that allow divs to be dragged
 	$(function() {
-	 	$( "#garbage" ).draggable();
+	 	$( "#bottle" ).draggable();
   	});
 
- 	$(" #jaw ").hover(function(){
-        $( "#garbage" ).hide();
+  	$(function() {
+	 	$( "#paper" ).draggable();
+  	});
+
+  	$(function() {
+	 	$( "#apple" ).draggable();
+  	});
+
+
+  	//function that hides divs when mouse is released over jaw allowing him to "eat"
+ 	$(" #bottle ").mouseup(function(){
+        $( "#bottle" ).hide();
     });
 
+ 	$(" #paper ").mouseup(function(){
+        $( "#paper" ).hide();
+    });
+
+    $(" #apple ").mouseup(function(){
+        $( "#apple" ).hide();
+    });
+
+
+ 	//mouse enter&leave function that opens and closes his mouth
 	$(" #jaw ").mouseenter(function() {
     	$(this).animate({ marginTop: -100 });
 
@@ -17,8 +38,19 @@ $(document).ready(function(){
 
 	$(" #jaw ").mouseleave(function() {
     	$(this).animate({ marginTop: -160 });
-
 	})
+
+
+ 	//mouth opening to vomit money 
+ 	//(code only above this- very long)
+	//setTimeout(function() {
+    		//$('#jaw').delay(4000).animate({ marginTop: -100 });
+    		//$('#money').delay(4000).fadeIn("slow");
+    		//$('#money').fadeOut("slow");
+    		//$('#jaw').delay(500).animate({ marginTop: -160 });
+
+	//})
+
 
 
 });
