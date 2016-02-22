4df8160e6671b45f48ccc60149388090c27d4e5d
mharris13 <mharris13@wou.edu>
2016-02-21 18:29:51 -0800

    2

--- project2/assets/js/main.js
+++ project2/assets/js/main.js
@@ -1,56 +1,77 @@
 $(document).ready(function(){
 
-	alert("Feed the Trump");
+      	alert("Feed the Trump");
 
-	//functions that allow divs to be dragged
-	$(function() {
-	 	$( "#bottle" ).draggable();
-  	});
+      	//functions that allow divs to be dragged
+      	$(function() {
+      	 	$( "#bottle" ).draggable();
+        	});
 
-  	$(function() {
-	 	$( "#paper" ).draggable();
-  	});
+        	$(function() {
+      	 	$( "#paper" ).draggable();
+        	});
 
-  	$(function() {
-	 	$( "#apple" ).draggable();
-  	});
-
-
-  	//function that hides divs when mouse is released over jaw allowing him to "eat"
- 	$(" #bottle ").mouseup(function(){
-        $( "#bottle" ).hide();
-    });
-
- 	$(" #paper ").mouseup(function(){
-        $( "#paper" ).hide();
-    });
-
-    $(" #apple ").mouseup(function(){
-        $( "#apple" ).hide();
-    });
+        	$(function() {
+      	 	$( "#apple" ).draggable();
+        	});
 
 
- 	//mouse enter&leave function that opens and closes his mouth
-	$(" #jaw ").mouseenter(function() {
-    	$(this).animate({ marginTop: -100 });
+        	//function that hides divs when mouse is released over jaw allowing him to "eat"
+       	$(" #bottle ").mouseup(function(){
+              $( "#bottle" ).hide();
+          });
 
-	})
+       	$(" #paper ").mouseup(function(){
+              $( "#paper" ).hide();
+          });
 
-	$(" #jaw ").mouseleave(function() {
-    	$(this).animate({ marginTop: -160 });
-	})
-
-
- 	//mouth opening to vomit money 
- 	//(code only above this- very long)
-	setTimeout(function() {
-    		$('#jaw').delay(4000).animate({ marginTop: -100 });
-    		$('#money').delay(4000).fadeIn("slow");
-    		$('#money').fadeOut("slow");
-    		$('#jaw').delay(500).animate({ marginTop: -160 });
-
-	})
+          $(" #apple ").mouseup(function(){
+              $( "#apple" ).hide();
+          });
 
 
+       	//mouse enter&leave function that opens and closes his mouth
+      	$(" #jaw ").mouseenter(function() {
+          	$(this).animate({ marginTop: -100 });
+
+      	})
+
+      	$(" #jaw ").mouseleave(function() {
+          	$(this).animate({ marginTop: -160 });
+      	})
+
+
+       	//mouth opening to vomit money 
+        //when you release the pieces of garbage, the trump throws up money
+        $(" #bottle ").mouseup(function(){
+              $('#jaw').animate({ marginTop: -100 });
+              $('#money').fadeIn("slow");
+              $('#money').fadeOut("slow");
+              $('#jaw').animate({ marginTop: -160 });
+        });
+
+        $(" #paper ").mouseup(function(){
+              $('#jaw').animate({ marginTop: -100 });
+              $('#money').fadeIn("slow");
+              $('#money').fadeOut("slow");
+              $('#jaw').animate({ marginTop: -160 });
+        });
+
+        $(" #apple ").mouseup(function(){
+              $('#jaw').animate({ marginTop: -100 });
+              $('#money').fadeIn("slow");
+              $('#money').fadeOut("slow");
+              $('#jaw').animate({ marginTop: -160 });
+        });
 
 });
+
+
+
+
+
+
+
+
+
+
